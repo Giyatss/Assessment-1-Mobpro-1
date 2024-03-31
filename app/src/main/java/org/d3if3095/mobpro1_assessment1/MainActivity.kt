ebac416576b69d47f84ad844af7aeb415a39c808
Giyats Almanfalutti <Almanfaluttigiyats@gmail.com>
2024-03-31 21:55:13 +0700

    progress1

--- app/src/main/java/org/d3if3095/mobpro1_assessment1/MainActivity.kt
+++ app/src/main/java/org/d3if3095/mobpro1_assessment1/MainActivity.kt
@@ -1,16 +1,27 @@
 package org.d3if3095.mobpro1_assessment1
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import org.d3if3095.mobpro1_assessment1.navigation.SetupNavGraph
+import org.d3if3095.mobpro1_assessment1.ui.screen.AboutScreen
 import org.d3if3095.mobpro1_assessment1.ui.theme.Mobpro1_assessment1Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +33,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SetupNavGraph()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(content: @Composable (Modifier) -> Unit){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { padding ->
+        content(Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    MainScreen { modifier ->
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScreenPreview() {
     Mobpro1_assessment1Theme {
-        Greeting("Android")
-    }
+        AboutScreen(rememberNavController())
+        }
 }
