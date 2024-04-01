495ad4eaddf617a55be43d70b714924579662744
Giyats Almanfalutti <Almanfaluttigiyats@gmail.com>
2024-04-01 11:04:00 +0700

    progress4

--- app/src/main/java/org/d3if3095/mobpro1_assessment1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3095/mobpro1_assessment1/ui/screen/MainScreen.kt
@@ -1,5 +1,7 @@
 package org.d3if3095.mobpro1_assessment1.ui.screen
 
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -24,10 +26,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
@@ -76,17 +80,20 @@ fun MainScreen(navController: NavHostController){
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
-    var merek by remember { mutableStateOf("") }
-    var tipe by remember { mutableStateOf("") }
-    var Rekomendasi1 by remember { mutableStateOf(false) }
-    var Rekomendasi2 by remember { mutableStateOf(false) }
-    var Rekomendasi3 by remember { mutableStateOf(false) }
-    var Rekomendasi4 by remember { mutableStateOf(false) }
+    var merek by rememberSaveable { mutableStateOf("") }
+    var tipe by rememberSaveable { mutableStateOf("") }
+    var Rekomendasi1 by rememberSaveable { mutableStateOf(false) }
+    var Rekomendasi2 by rememberSaveable { mutableStateOf(false) }
+    var Rekomendasi3 by rememberSaveable { mutableStateOf(false) }
+    var Rekomendasi4 by rememberSaveable { mutableStateOf(false) }
+
+    var isInputValid by rememberSaveable { mutableStateOf(true) }
 
     val radioOptions = listOf(
         stringResource(id = R.string.mobil_manual),
         stringResource(id = R.string.mobil_matic)
     )
+    val context = LocalContext.current
 
     Column (
         modifier = modifier
@@ -118,7 +125,9 @@ fun ScreenContent(modifier: Modifier) {
 
         // Radio buttons for tipe mobil
         Row(
-            modifier = Modifier.padding(top = 6.dp).border(1.dp, Color.Gray, RoundedCornerShape(4.dp)),
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp)),
             verticalAlignment =  Alignment.CenterVertically
         ) {
             radioOptions.forEach { text ->
@@ -130,7 +139,8 @@ fun ScreenContent(modifier: Modifier) {
                                 tipe = text
                             },
                             role = Role.RadioButton
-                        ).weight(1f)
+                        )
+                        .weight(1f)
                         .padding(16.dp)
                 ) {
                     RadioButton(selected = tipe == text, onClick = null)
@@ -142,28 +152,33 @@ fun ScreenContent(modifier: Modifier) {
         // Button to show recommendations
         Button(
             onClick = {
+                // Reset input validation
+                isInputValid = true
 
-                Rekomendasi1 = false
-                Rekomendasi2 = false
-                Rekomendasi3 = false
-                Rekomendasi4 = false
-
-                if (merek.lowercase() == "toyota" && tipe.lowercase() == "matic") {
-                    Rekomendasi1 = true
-                }
-                if (merek.lowercase() == "toyota" && tipe.lowercase() == "manual") {
-                    Rekomendasi2 = true
-                }
-                if (merek.lowercase() == "daihatsu" && tipe.lowercase() == "matic") {
-                    Rekomendasi3 = true
-                }
-                if (merek.lowercase() == "daihatsu" && tipe.lowercase() == "manual") {
-                    Rekomendasi4 = true
+                // Validate input
+                if (merek.isEmpty() || tipe.isEmpty()) {
+                    isInputValid = false
+                } else {
+                    // Show recommendations based on input
+                    Rekomendasi1 = (merek.lowercase() == "toyota" && tipe.lowercase() == "matic")
+                    Rekomendasi2 = (merek.lowercase() == "toyota" && tipe.lowercase() == "manual")
+                    Rekomendasi3 = (merek.lowercase() == "daihatsu" && tipe.lowercase() == "matic")
+                    Rekomendasi4 = (merek.lowercase() == "daihatsu" && tipe.lowercase() == "manual")
                 }
             },
             modifier = Modifier.padding(top = 20.dp)
         ) {
             Text(text = stringResource(R.string.lihat_rekomendasi))
+        }
+
+        // Show input validation message if input is not valid
+        if (!isInputValid) {
+            Text(
+                text = "Input tidak valid, mohon isi kedua inputan.",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Red,
+                modifier = Modifier.padding(top = 8.dp)
+            )
         }
 
         // Recommendations section
@@ -180,13 +195,27 @@ fun ScreenContent(modifier: Modifier) {
             DaihatsuManual()
         }
 
+
+    }
+}
+private fun shareData(context: Context, massage: String) {
+    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, massage)
+    }
+    if(shareIntent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(shareIntent)
     }
 }
 
 @Composable
 fun ToyotaMatic() {
+    val context = LocalContext.current
+
     Column(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 50.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 50.dp)
     ) {
         Text(text = stringResource(id = R.string.toyota_matic),
             textAlign = TextAlign.Center,
@@ -203,12 +232,27 @@ fun ToyotaMatic() {
         Text(text = stringResource(id = R.string.isi_toyota_matic),
             modifier = Modifier.padding(top = 40.dp))
     }
+    Button(
+        onClick = {
+            shareData(
+                context = context,
+                massage = context.getString(R.string.bagikan).uppercase())
+        },
+        modifier = Modifier.padding(top = 8.dp),
+        contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+    ) {
+        Text(text = stringResource(R.string.bagikan))
+    }
 }
 
 @Composable
 fun ToyotaManual() {
+    val context = LocalContext.current
+
     Column(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 50.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 50.dp)
     ) {
         Text(text = stringResource(id = R.string.toyota_manual),
             textAlign = TextAlign.Center,
@@ -225,12 +269,27 @@ fun ToyotaManual() {
         Text(text = stringResource(id = R.string.isi_toyota_manual),
             modifier = Modifier.padding(top = 40.dp))
     }
+    Button(
+        onClick = {
+            shareData(
+                context = context,
+                massage = context.getString(R.string.bagikan).uppercase())
+        },
+        modifier = Modifier.padding(top = 8.dp),
+        contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+    ) {
+        Text(text = stringResource(R.string.bagikan))
+    }
 }
 
 @Composable
 fun DaihatsuMatic() {
+    val context = LocalContext.current
+
     Column(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 50.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 50.dp)
     ) {
         Text(text = stringResource(id = R.string.daihatsu_matic),
             textAlign = TextAlign.Center,
@@ -248,12 +307,27 @@ fun DaihatsuMatic() {
         Text(text = stringResource(id = R.string.isi_daihatsu_matic),
             modifier = Modifier.padding(top = 40.dp))
     }
+    Button(
+        onClick = {
+            shareData(
+                context = context,
+                massage = context.getString(R.string.bagikan).uppercase())
+        },
+        modifier = Modifier.padding(top = 8.dp),
+        contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+    ) {
+        Text(text = stringResource(R.string.bagikan))
+    }
 }
 
 @Composable
 fun DaihatsuManual() {
+    val context = LocalContext.current
+
     Column(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 50.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 50.dp)
     ) {
         Text(text = stringResource(id = R.string.daihatsu_manual),
             textAlign = TextAlign.Center,
@@ -270,6 +344,17 @@ fun DaihatsuManual() {
         )
         Text(text = stringResource(id = R.string.isi_daihatsu_manual),
             modifier = Modifier.padding(top = 40.dp))
+    }
+    Button(
+        onClick = {
+            shareData(
+                context = context,
+                massage = context.getString(R.string.bagikan).uppercase())
+        },
+        modifier = Modifier.padding(top = 8.dp),
+        contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+    ) {
+        Text(text = stringResource(R.string.bagikan))
     }
 }
 
